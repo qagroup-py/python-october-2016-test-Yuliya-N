bd09e7d2117c6a25da8c27c7cd5a73a503b6127b
Yuliya-N <yuliya.nykytenko@gmail.com>
2016-10-22 12:51:35 +0300

    Add empty classes with hierarchy in OOP_food.py

--- OOP_food.py
+++ OOP_food.py
@@ -18,3 +18,100 @@ Red globe (grapes)  (виноград "Ред Глоб")
 Victoria (grapes)  (виногдар "Вікторія")
 Wild strawberry  (суниця)
 """
+
+
+class Plant():
+    pass
+
+
+class Vegetable(Plant):
+    pass
+
+
+class Fruit(Plant):
+    pass
+
+
+class Herb(Plant):
+    pass
+
+
+class Basil(Herb):
+    pass
+
+
+class Tuber(Vegetable):
+    pass
+
+
+class Beetror(Tuber):
+    pass
+
+
+class Cabbage(Vegetable):
+    pass
+
+
+class Carrot(Tuber):
+    pass
+
+
+class Pear(Fruit):
+    pass
+
+
+class Concorde(Pear):
+    pass
+
+
+class Dill(Herb):
+    pass
+
+
+class Berry(Fruit):
+    pass
+
+
+class Strawberry(Berry):
+    pass
+
+
+class DomesticStrawberry(Strawberry):
+    pass
+
+
+class Apple(Fruit):
+    pass
+
+
+class GoldenDelicious(Apple):
+    pass
+
+
+class Granny Smich(Apple):
+    pass
+
+
+class WildStrawberry(Strawberry):
+    pass
+
+
+class Onion(Vegetable):
+    pass
+
+
+class Potato(Tuber):
+    pass
+
+
+class Grape(Fruit):
+    pass
+
+
+
+class RedGlobes(Grape):
+    pass
+
+
+class Victoria(Grape):
+    pass
